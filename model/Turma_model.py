fc9c4abec07f749c01fbabf5582ed63878c604a4
Kauan Marabotti <Kauanmarabotti@gmail.com>
2025-10-21 20:47:38 -0300

    correcao turma

--- model/Turma_model.py
+++ model/Turma_model.py
@@ -2,7 +2,8 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
 
-from model.Atividade_model import Atividade
+
+from model.atividade_model import Atividade
 from model.usuario_model import Usuario
 
 
