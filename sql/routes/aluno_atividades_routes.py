04f399d537a8c3ddb3e451912f9836da2357031e
Rafael Rebonato <rafaelrebonato04@gmail.com>
2025-10-21 21:05:40 -0300

    routes aluno atividades

---
+++ sql/routes/aluno_atividades_routes.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Request
+from util.auth_decorator import requer_aluno
+from util.template_util import criar_templates
+from repo import atividade_repo
+
+router = APIRouter(prefix="/aluno/atividades")
+templates = criar_templates("templates/aluno/atividades")
+
+@router.get("")
+@requer_aluno
+async def listar_atividades(request: Request):
+    """Lista atividades disponíveis"""
+    atividades = atividade_repo.obter_todas()
+    return templates.TemplateResponse("lista.html", {
+        "request": request,
+        "atividades": atividades
+    })
+
+@router.get("/{id}")
+@requer_aluno
+async def ver_atividade(request: Request, id: int):
+    """Detalhes de uma atividade"""
+    atividade = atividade_repo.obter_por_id(id)
+    return templates.TemplateResponse("detalhes.html", {
+        "request": request,
+        "atividade": atividade
+    })
